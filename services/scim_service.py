6dc84a43d054729402a02c7d186d38e256c305a6
Charles Jacquemus <charles.jacquemus@gmail.com>
2026-02-27 01:30:24 +0100

    ajout de la pagination dans scim

--- services/scim_service.py
+++ services/scim_service.py
@@ -27,7 +27,9 @@ class ScimService:
         soup = BeautifulSoup(response.text, "html.parser")
         cards = soup.find_all("div", class_="card-body")
 
-        results = []
+        pagination_maxpage = int(soup.find("ul", class_="pagination").find_all("li")[-1].find("a").get("href", "0").split("/")[-1])
+
+        results = type('',(object,),{"items": [],"maxPage":pagination_maxpage})()
 
         for card in cards:
 
@@ -60,7 +62,7 @@ class ScimService:
             # description = self.get_description(blueprint_id)
             
 
-            results.append({
+            results.items.append({
                 "id": blueprint_id,
                 "title": title,
                 "description": None,
---
+++ utils/paginatator.py
@@ -0,0 +1,46 @@
+from typing import Callable
+
+# parce qu'il gere une pagination presque comme une patator
+class Paginatator():
+    def __init__(self):
+        self.minPage=1
+        self.maxPage=10
+        self.currentPage=1
+        self.width=10
+        self.calc()
+
+    def setMinPage(self, _minPage:int):
+        self.minPage=_minPage
+        self.calc()
+
+    def setMaxPage(self, _maxPage:int):
+        self.maxPage=_maxPage
+        self.calc()
+
+    def setWidth(self, _width:int):
+        self.width=_width
+        self.calc()
+
+    def setCurrentPage(self, _currentPage:int):
+        self.currentPage=min(self.maxPage, max(self.minPage,_currentPage))
+        self.calc()
+
+    def calc(self):
+        self.begin = max(min(int(self.currentPage-self.width/2), self.maxPage-self.width),self.minPage)
+        self.end = min(self.begin + self.width, self.maxPage)
+
+    def generateFirst(self, generator:Callable[[int], any]):
+        return generator(self.minPage)
+    
+    def generatePrevious(self, generator:Callable[[int], any]):
+        return generator(max(self.minPage, self.currentPage-1))
+    
+    def generateNext(self, generator:Callable[[int], any]):
+        return generator(min(self.maxPage, self.currentPage+1))
+    
+    def generateLast(self, generator:Callable[[int], any]):
+        return generator(self.maxPage)
+    
+    def generateRange(self, generator:Callable[[int, int], any]):
+        return list(map(lambda i:generator(i,self.currentPage), range(self.begin, self.end)))
+    
